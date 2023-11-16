5be0a47eb8935ec71a2f3e9dff4f085cd0bfb37d
Stefan Spangenberg <stefan.spangenberg@student.hpi.de>
2023-11-16 10:25:40 +0100

    Add docker compose setup

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-params="$*"
-prompt="$params minimal flat 2d vector icon. lineal color. on a white background. trending on artstation."
-python3 run_painterly_render.py -c vectorfusion.yaml -pt "$prompt" -save_step 10 -respath /tmp/workdir -d 8888 --download -update "K=4 style='iconography' sds.num_iter=1000"
