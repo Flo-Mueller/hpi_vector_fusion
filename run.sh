8a16533e3130921917b44f565ba8d80303e3f87d
LeonHermann322 <leon.hermann@me.com>
2023-11-28 04:48:09 +0100

    setup failing with unsupported toolchain

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 prefix_prompt=$1
-suffix_prompt="minimal flat 2d vector icon. lineal color. on a white background. trending on artstation."
+suffix_prompt="minimal 2d line drawing. trending on artstation."
 prompt="$prefix_prompt $suffix_prompt"
-overrides="style='iconography' sds.num_iter=500 num_iter=50"
+#overrides="style=iconography sds.num_iter=500 num_iter=50 image_size=300"
 
-#docker run --rm --name="vectorfusion_leon" -v "$(pwd):/tmp/workdir" --gpus '"device=1"' vectorfusion_leon:latest python3 run_painterly_render.py -c vectorfusion.yaml -pt "$prompt" -save_step 10 -respath /tmp/workdir -d 8888 --download -update $overrides
-python3 run_painterly_render.py -c vectorfusion.yaml -pt "$prompt" -save_step 10 -respath /tmp/workdir -d 8888 --download -update $overrides
+cd VectorFusion-pytorch
+CUDA_VISIBLE_DEVICES=0 python3 run_painterly_render.py -c vectorfusion.yaml -pt "$prompt" -save_step 10 -mv -frame_freq 10 -respath /tmp/workdir -d 8888 --download #-update $overrides
