d850618577624676793d900da4d3f7738ffc6172
LeonHermann322 <leon.hermann@me.com>
2023-11-28 05:01:56 +0100

    removing unnecessary changes

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-prefix_prompt=$1
-suffix_prompt="minimal 2d line drawing. trending on artstation."
-prompt="$prefix_prompt $suffix_prompt"
-#overrides="style=iconography sds.num_iter=500 num_iter=50 image_size=300"
-
-cd VectorFusion-pytorch
-CUDA_VISIBLE_DEVICES=0 python3 run_painterly_render.py -c vectorfusion.yaml -pt "$prompt" -save_step 10 -mv -frame_freq 10 -respath /tmp/workdir -d 8888 --download #-update $overrides
+params="$*"
+prompt="$params minimal flat 2d vector icon. lineal color. on a white background. trending on artstation."
+python3 run_painterly_render.py -c vectorfusion.yaml -pt "$prompt" -save_step 10 -respath /tmp/workdir -d 8888 --download -update "K=4 style='iconography' sds.num_iter=1000"
